ad86da4315de8d1f17ae1618cbcfcdb7ca35eab3
Kauansct <kauan.tedesco@hotmail.com>
2024-08-27 21:15:42 -0300

    primeiro commit da loucura

---
+++ loucura.js
@@ -0,0 +1,7 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null // proximo elemento da lista
+        this.previous = null // elemento anterior da lista
+    }
+}
